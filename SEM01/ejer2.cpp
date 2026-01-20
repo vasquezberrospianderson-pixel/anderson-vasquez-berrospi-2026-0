1d3aeb58b4c5f5b1767843419d8e3b423a725c3a
andeson aldair vasquez berrospi <vasquezberrospianderson@gmail.com>
2026-01-19 21:06:55 -0500

    v1.1

---
+++ SEM01/ejer2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int edad(){
+    int edad;
+cout<< "ingrese su edad: ";
+cin>> edad;
+cout<< "su edad es: "<< edad;
+    return 0;
+}
---
+++ SEM01/python/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print("Estoy aprendiendo c++")
