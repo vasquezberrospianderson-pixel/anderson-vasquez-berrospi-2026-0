2f0bb2aa90c25e5f2157aa89d3c707ec8bb75c92
andeson aldair vasquez berrospi <vasquezberrospianderson@gmail.com>
2026-02-04 17:55:44 -0500

    Actualización del proyecto

--- SEM01/Taller Viernes/tempCodeRunnerFile.cpp
+++ SEM01/Taller Viernes/tempCodeRunnerFile.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <math.h>
 int main() {
@@ -12,24 +11,5 @@ int main() {
     printf("Area: %.2f\n", area);
     printf("Perimetro: %.2f\n", perimetro);
     printf("Diagonal: %.2f\n", diagonal);
-=======
-#include <iostream>
-#include <stdio.h>
-#include <cmath>
-using namespace std;
-int main(){
-    float a,b,c,p,S;
-
-    a = 3;
-    b = 7;
-    c = 5;
-    p = (a+b+c)/2;
-    S = sqrt(p*(p-a)*(p-b)*(p-c));
-    cout<<"lado1 = "<<a<<endl;
-    cout<<"lado2 = "<<b<<endl;
-    cout<<"lado3 = "<<c<<endl;
-    cout<<"semi perimetro = "<<p<<endl;
-    cout<<"area = "<<S<<endl;
->>>>>>> 6e4683b39acd7485c8f5e774ff1d2d2691194926
     return 0;
 }
--- SEM01/ejer5.cpp
+++ SEM01/ejer5.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
     float a,b,c,prom;
-=======
-    int a,b,c,prom;
->>>>>>> 6e4683b39acd7485c8f5e774ff1d2d2691194926
 cout<<"ingrese el primer numero: ";
 cin>>a;
 cout<<"ingrese el segundo numero: ";
